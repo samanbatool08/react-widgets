3b5e3f37ce76187b818507f17a4e19d7f67db2ba
Saman Batool <samanbatool08@gmail.com>
2022-10-21 14:26:24 -0400

    conditionally render options - option 2

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -23,11 +23,11 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   <div className='ui form'>
     <div className='field'>
       <label className='label'>Select a Color</label>
-      <div onClick={() => setOpen(!open)} className='ui selection dropdown visible active'>
+      <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
         <i className='dropdown icon'></i>
         <div className='text'>{selected.label}</div>
-        <div className='menu visible transition'>
-          {open ? renderOptions : null}
+        <div className={`menu ${open ? 'visible transition' : ''}`}>
+          {renderOptions}
         </div>
       </div>
     </div>   
