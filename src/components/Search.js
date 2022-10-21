5e05d938207af152c1a2bc89a7d12a26f81c30b6
Saman Batool <samanbatool08@gmail.com>
2022-10-21 10:49:38 -0400

    setup search form

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+const Search = () => {
+  return <div>
+    <div className="ui form">
+      <div className="field">
+        <label>Enter Search Term</label>
+        <input className="input"/>
+      </div>
+    </div>
+  </div>
+}
+
+export default Search; 
