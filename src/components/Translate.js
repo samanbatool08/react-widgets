cec9b195d979a6d9038d26236191a32485989e4b
Saman Batool <samanbatool08@gmail.com>
2022-10-21 17:39:06 -0400

    successfuly using google translate api

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,8 @@
+// google translate api key AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+
 import React, { useState } from 'react'; 
 import Dropdown from './Dropdown';
+import Convert from './Covert';
 
 
 const options = [
@@ -24,13 +27,25 @@ const Translate = () => {
 
   return (
     <div>
-
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
       <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
+
       <Dropdown 
       label="Select Language"
       options={options}
       selected={language}
       onSelectedChange={setLanguage}
+      />
+
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert 
+        text={text}
+        language={language}   
       />
     </div>
   )
